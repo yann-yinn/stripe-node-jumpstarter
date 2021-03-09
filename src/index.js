dbff38b501af4942d5410c612efb559282c1e516
yann <yann.boisselier@gmail.com>
2021-03-09 10:21:22 +0100

    fix userManagement for stripe routes

--- src/index.js
+++ src/index.js
@@ -1,54 +1,18 @@
-require("dotenv").config();
-const express = require("express");
-const stripeRoutes = require("./stripe/routes");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const userManagement = require("./utils/userManagement");
-const { connect, db } = require("./utils/db");
+const bootstrap = require("./utils/bootstrap");
 ObjectId = require("mongodb").ObjectID;
 
-// configurer notre serveur HTTP
-const app = express();
-app.use(cors());
+bootstrap().then(({ app }) => {
+  // ajouter nos routes stripes
+  app.use(require("./stripe/routes"));
 
-// se connecter à notre base de données Mongo
-connect();
+  app.get("/userinfo", async (req, res) => {
+    const user = await db()
+      .collection("users")
+      .findOne({ _id: ObjectId(req.user.id) });
+    res.send({ user: user });
+  });
 
-// démarrer l'API express pour l'authentification
-(async () => {
-  await userManagement.init(app);
-})();
-
-app.use((req, res, next) => {
-  // ne PAS utiliser le parser JSON automatiquement pour les webhooks!
-  if (req.originalUrl === "/api/stripe/webhooks") {
-    next();
-  } else {
-    bodyParser.json({ limit: "5mb" })(req, res, next);
-  }
-});
-
-/**
- * Ajouter les routes d'API pour Stripe
- */
-app.use(stripeRoutes);
-
-app.get("/", (req, res) => {
-  res.send({ status: "running" });
-});
-
-app.get("/userinfo", async (req, res) => {
-  const user = await db()
-    .collection("users")
-    .findOne({ _id: ObjectId(req.user.id) });
-  res.send({ user: user });
-});
-
-/**
- * Servir notre front-end (un build est obligatoire avant de pouvoir le servir)
- */
-app.use(express.static("front/dist"));
-
-app.listen(process.env.PORT, () => {
-  console.log(`server app listening on port ${process.env.PORT}!`);
+  app.listen(process.env.PORT, () => {
+    console.log(`server app listening on port ${process.env.PORT}!`);
+  });
 });
--- src/stripe/routes/index.js
+++ src/stripe/routes/index.js
@@ -6,16 +6,19 @@ const stripeController = require("../controllers/index");
 const bodyParser = require("body-parser");
 const express = require("express");
 const router = express.Router();
+const userManagement = require("../../utils/userManagement");
 
 router.get("/api/stripe/plans", stripeController.plans);
 
 router.post(
   "/api/stripe/create-checkout-session",
+  userManagement.get().auth.required,
   stripeController.createCheckoutSession
 );
 
 router.post(
   "/api/stripe/create-customer-portal-session",
+  userManagement.get().auth.required,
   stripeController.createCustomerPortalSession
 );
 
